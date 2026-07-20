ca88c6342481c6819f9c326947b9b6c77f597611
Axygen4oO <rolfik.ololo@yandex.ru>
2026-07-20 03:31:03 +0300

    New version 1.3.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "net.qwdtt.client"
         minSdk = 28
         targetSdk = 35
-        versionCode = 33
-        versionName = "1.3.7"
+        versionCode = 35
+        versionName = "1.3.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
