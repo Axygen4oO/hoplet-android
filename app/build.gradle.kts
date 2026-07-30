bc23385923fa2a2ac5ac1c75ce5cfe58e09da966
Axygen4oO <rolfik.ololo@yandex.ru>
2026-07-31 00:08:36 +0300

    v

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "net.qwdtt.client"
         minSdk = 28
         targetSdk = 35
-        versionCode = 40
-        versionName = "1.4.3"
+        versionCode = 41
+        versionName = "1.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
